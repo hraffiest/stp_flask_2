643d1b8f10e30034a629f280c815b35b1fb75e10
Hrafnir <g@tgg.su>
2020-03-20 15:46:23 +0300

    add config.py

--- src/app.py
+++ src/app.py
@@ -1,13 +1,19 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from config import Config
 from flask_migrate import Migrate
 from flask_wtf import FlaskForm
 from wtforms import StringField
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-db = SQLAlchemy(app)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.secret_key = 'asdwefwedf'
+db = SQLAlchemy(app)
+admin = Admin(app)
 
 
 if __name__ == '__main__':
