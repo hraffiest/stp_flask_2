b45cc81d4ac26bed27e8ae6b9e2825ffc3b7cedd
Hrafnir <g@tgg.su>
2020-03-21 20:14:19 +0300

    session.cart is ready

--- views.py
+++ views.py
@@ -1,7 +1,14 @@
 from __init__ import app, db
 from models import *
 import json
-from flask import abort, flash, render_template, request, redirect
+from flask import abort, flash, render_template, request, redirect, session
+
+
+def get_cart_info(ids):
+    cart = []
+    for id in ids:
+        cart.append(db.session.query(Dish).get(id).price)
+    return [len(ids), sum(cart)]
 
 
 #
@@ -23,6 +30,10 @@ from flask import abort, flash, render_template, request, redirect
 @app.route('/')
 def home():
     # cook the dict 'dishes_d' for main page
+    cart = session.get("cart")
+    cart_info = [0, 0]
+    if cart:
+        cart_info = get_cart_info(session['cart'])
     dishes_d = dict()
     cats = db.session.query(Category).order_by(Category.c_id).all()
     dishes = db.session.query(Dish).order_by(Dish.cat_id).all()
@@ -31,15 +42,23 @@ def home():
         for dish in dishes:
             if cat.c_id == dish.cat_id:
                 dishes_d[cat.title].append(dish)
-    return render_template('main.html', dishes_d=dishes_d)
+    return render_template('main.html', dishes_d=dishes_d, cart_info=cart_info)
 
-#
-# # ------------------------------------------------------
-# # для корзины
-# @app.route("/cart/", methods=["GET", "POST"])
-# def show_the_cart():
-#     pass
-#     # (код страницы для корзины)
+
+@app.route("/addcart/<int:d_id>", methods=['POST'])
+def add_to_cart(d_id):
+    cart = session.get("cart", [])
+    cart.append(d_id)
+    session['cart'] = cart
+    return redirect('/cart/')
+
+
+# ------------------------------------------------------
+# для корзины
+@app.route("/cart/", methods=['GET', 'POST'])
+def show_the_cart():
+    pass
+    # (код страницы для корзины)
 #
 #
 # # ------------------------------------------------------
